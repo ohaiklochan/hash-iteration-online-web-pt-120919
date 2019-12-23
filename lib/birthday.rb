a29e3003262cb5691b6e880d58c816290308354b
Chloe Foronda <caforonda103@yahoo.com>
2019-12-23 02:52:53 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,6 +13,8 @@ end
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age < 12
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else
+      puts "You're too old for this."
   end
 end
