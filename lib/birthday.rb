a2cd234df5b16cae62581bbdf5e155414d63c643
Chloe Foronda <caforonda103@yahoo.com>
2019-12-23 02:32:51 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,7 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    puts "Happy Birthday #{kids_name}! Happy You are now #{age} years old!"
   end
 end
 
